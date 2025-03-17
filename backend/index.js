6725cf1aaf1f3027f7707b5c0dfffdeffe660b3e
KrittiderPJ <krittidet2545@gmail.com>
2025-03-17 20:26:37 +0700

    create product model

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
-import express from 'express';
+import express, { json } from 'express';
 import { connectDB } from './congig/db.js';
+import Product from './models/product.model.js';
 
 const app = express();
 
-app.get('/products', (req, res) => {
-     
+app.use(express.json()) // allow us to accept JSON data in the req.body
+
+app.post('/api/products', async (req, res) => {
+    const product = req.body // user send this data
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all field" })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(201).json({ success: true, data: newProduct})
+    } catch (error) {
+        console.error("Error in create Product", error)
+        res.status(500).json({ success: false, message: "Server Error"}) // status code 500 for inner server error
+    }
 })
 
 app.listen(5000, () => {
