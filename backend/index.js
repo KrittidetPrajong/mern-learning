aadd9609a57270406ac1400ac5b79f12dda75a6d
KrittiderPJ <krittidet2545@gmail.com>
2025-03-18 01:19:51 +0700

    implement api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,37 @@
 import express, { json } from 'express';
 import { connectDB } from './congig/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json()) // allow us to accept JSON data in the req.body
+
+app.put('/api/products/:id', async (req, res) => {
+    const {id} = req.params
+    const product = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid ID" })
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true})
+        res.status(200).json({ success: true, data: updatedProduct })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+})
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({}) // fetch all products in db
+        res.status(200).json({ success: true, data: products })
+    } catch (error) {
+        console.log("Error in fetching products: ", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+}) 
 
 app.post('/api/products', async (req, res) => {
     const product = req.body // user send this data
@@ -21,6 +48,18 @@ app.post('/api/products', async (req, res) => {
     } catch (error) {
         console.error("Error in create Product", error)
         res.status(500).json({ success: false, message: "Server Error"}) // status code 500 for inner server error
+    }
+})
+
+app.delete('/api/products/:id', async (req, res) => {
+    const {id} = req.params
+
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({success: true, message: "Delete Product"})
+    } catch (error) {
+        console.log("Error in deleting product: ", error.message)
+        res.status(404).json({success: false, message: "Product Not Found"})
     }
 })
 
