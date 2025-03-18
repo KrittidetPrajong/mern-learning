fa69d68cc4ec4144fba37e75460ad19f091db9bc
KrittiderPJ <krittidet2545@gmail.com>
2025-03-18 22:21:36 +0700

    implement controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,63 +1,14 @@
 import express from 'express'
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/product.controller.js';
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find({}) // fetch all products in db
-        res.status(200).json({ success: true, data: products })
-    } catch (error) {
-        console.log("Error in fetching products: ", error.message)
-        res.status(500).json({ success: false, message: "Server Error" })
-    }
-})
+router.post('/', createProduct)
+
+router.get('/', getProducts)
  
-router.post('/', async (req, res) => {
-    const product = req.body // user send this data
+router.put('/:id', updateProduct)
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please provide all field" })
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save()
-        res.status(201).json({ success: true, data: newProduct})
-    } catch (error) {
-        console.error("Error in create Product", error)
-        res.status(500).json({ success: false, message: "Server Error"}) // status code 500 for inner server error
-    }
-})
-
-router.put('/:id', async (req, res) => {
-    const {id} = req.params
-    const product = req.body
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Invalid ID" })
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true})
-        res.status(200).json({ success: true, data: updatedProduct })
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Server Error" })
-    }
-})
-
-router.delete('/:id', async (req, res) => {
-    const {id} = req.params
-
-    try {
-        await Product.findByIdAndDelete(id)
-        res.status(200).json({success: true, message: "Delete Product"})
-    } catch (error) {
-        console.log("Error in deleting product: ", error.message)
-        res.status(404).json({success: false, message: "Product Not Found"})
-    }
-})
+router.delete('/:id', deleteProduct)
 
 export default router
